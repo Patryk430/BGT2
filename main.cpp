44ddb97841fe68d55b6086816d3a93254fbdb3cc
Patryk430 <patryk.survilo@mif.stud.vu.lt>
2025-10-23 19:53:09 +0300

    user generation

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include "hash.h"
+#include "realistic_names.cpp"
 #include <chrono>
 #include <list>
+#include <iomanip>
 
 class User
 {
     private:
 
     const std::string name;
-    const std::string public_key = name; // temp
+    const std::string public_key = name + "'s_key"; // temp
     unsigned int balance = 30; // temp
 
     public:
 
     std::string get_name() {return name;}
+    std::string get_key() {return public_key;}
     unsigned int get_balance() {return balance;}
 
+    void print_about_me() {std::cout << "Name: " << std::setw(25) << name << " | Balance: " << std::setw(10) << balance << " PatCoins | Public Key: " << public_key << "\n";}
+
     void update_balance(int update) {balance=balance+update;}
-    User(std::string name) : name(name) {}
+    User(std::string name, unsigned int balance) : name(name), balance(balance) {}
 
 
 };
@@ -33,7 +38,7 @@ class Transaction
     const std::string transaction_id = Hash (sender + receiver + std::to_string(amount));
 
     public:
-    Transaction (User& sender, User& receiver, unsigned int amount) : sender(sender.get_name()), receiver(receiver.get_name()), amount(amount) {sender.update_balance(-amount), receiver.update_balance(amount);}
+    Transaction (User& sender, User& receiver, unsigned int amount) : sender(sender.get_key()), receiver(receiver.get_key()), amount(amount) {sender.update_balance(-amount), receiver.update_balance(amount);}
 
     void print() {std::cout << transaction_id << ": "<< sender << " >>>> " << receiver << " | " << amount << " PatCoin\n";}
 };
@@ -54,10 +59,33 @@ class Block
     public:
 };
 
+std::vector<User> Generate_Users() 
+{
+    std::vector<User> Users;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(100, 1000000);
+    
+    for (int i = 0; i < 1000; i++)
+    {
+        Users.push_back (User (names[i], distr(gen)));
+    }
+
+    return Users;
+} 
+
 int main() 
 {
-    User a ("Andrius");
-    User b ("Paulius");
+    std::vector <User> Users = Generate_Users();
+
+    for (User u : Users)
+    {
+        u.print_about_me();
+    }
+
+    User a ("Andrius", 100);
+    User b ("Paulius", 40);
 
     std::cout << a.get_balance() << " " << b.get_balance() << "\n";
 
